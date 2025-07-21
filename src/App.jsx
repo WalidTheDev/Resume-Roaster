aabf7b9bc335dbefeb021c17c8654b4e939e35bb
Walid Shaikh <walidshaikh552@gmail.com>
2025-07-21 18:59:05 +0530

    set up

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+function App() {
+
+  // const [resume , setResume] = useState("");
+  // const api = import.meta.env.VITE_GEMINI_API_KEY;
+  // function handleSubmit(e){
+
+  // }
+
+  return (
+    <>
+      {/* <h1>Resume Roaster , {api}</h1> */}
+      <div className="w-[100vw] h-[100vh] bg-gray-300 flex justify-center items-center">
+        <div className="from-card w-[20vw] bg-gray-700 h-[30%] rounded-2xl p-5">
+           
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
